0e026a0ddb073949007051bfa7761ad11c4ee3cb
Artem Kiyashko <artem86@hotmail.co.uk>
2018-02-12 13:57:22 +0100

    Console app for local debugging added

---
+++ SezBot3000v2_Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SezBot3000v2.Services;
+using System;
+
+namespace SezBot3000v2_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+
+            Startup startup = new Startup(builder.Build());
+
+            startup.ConfigureServices(services);
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Debug);
+
+            var service = serviceProvider.GetService<IUpdateService>();
+
+            Console.ReadKey();
+        }
+    }
+}
